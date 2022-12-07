2d5bd1659ff0a4bd690b0d900c765cea3b535a5e
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-12-07 18:16:56 +0530

    updating ./terraform/tf-module-mutable-lb

--- vars.tf
+++ vars.tf
@@ -5,7 +5,8 @@ variable "PUBLIC_SUBNET_IDS" {}
 variable "PRIVATE_SUBNET_IDS" {}
 variable "ALLOW_SG_CIDR" {}
 variable "VPC_ID" {}
-
+variable "PUBLIC_ZONE_ID" {}
+variable "PRIVATE_ZONE_ID" {}
 
 
 
